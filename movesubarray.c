5d87bb02887a0c070cf09bf5995a9cbeaf3bce35
gsong <gnbbgys@gmail.com>
2017-01-08 12:29:51 -0500

    update

--- movesubarray.c
+++ movesubarray.c
@@ -31,11 +31,27 @@ void movesubarray1(int arr[], int sz, int pos)
 }
 
 //mod of pos, circular way to do it
-//delicate method
 void movesubarray2(int arr[], int sz, int pos)
 {
+    int tmp   = arr[0];
+    int idx   = 0; //current position
+    int count = 0; //total data moved
+    int start_idx = 0; //starting pos of current loop
 
+    while(count < sz-1)
+    {
+        if(idx = start_idx){ //loop end, but not finish move
+            start_idx += 1;
+            idx = start_idx;
+        }
+
+        arr[idx] = arr[idx+pos]; 
+        idx = (idx+pos) % pos;
+        count++;
+    }
 }
+
+//recursive method
 
 //use reverse 
 void movesubarray3(int arr[], int sz, int pos)
