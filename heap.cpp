6072d7a20f81ecc8519e552ab31e0e1419900afe
gnbbgys <gnbbgys@gmail.com>
2017-02-26 17:55:00 -0500

    up

---
+++ heap.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+
+
+void heapify(int a[], int n, int r)
+{
+    
+}
+
+void build_heap(int a[], int n)
+{
+    for(int i = n/2; i > 0; --i)
+    {
+        heapify(a, n, i); 
+    }
+}
+
+int heap_compare(int a[], int idx, int count, int v)
+{
+    
+}
+
+int main()
+{
+    return 0;
+}
+
