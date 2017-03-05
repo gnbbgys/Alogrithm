5cffd6c3813440bef2e08d1c3246068de467e88d
gnbbgys <gnbbgys@gmail.com>
2017-03-05 16:01:15 -0500

    update

--- merge.cpp
+++ merge.cpp
@@ -9,6 +9,7 @@ void print_matrix(int** a, int k, int p)
         {
             std::cout << a[idx][pos] << " "; 
         }
+
         std::cout << std::endl; 
     }
 }
@@ -22,6 +23,7 @@ void print_array(int a[], int n)
     std::cout << std::endl; 
 }
 
+/*
 int find_min(int a[][], int p[], int k, int n)
 {
     int min = a[0][p[0]];
@@ -94,40 +96,9 @@ enum{
     ARGUMENT_ERROR
 };
 
-int parent(int idx)
-{
-    if(idx % 2 == 0) return (idx-1)/2;
-    else return idx/2;
-}
-
-void swap(int& a, int& b)
-{
-    int tmp = a;
-    b = a;
-    a = tmp;
-}
-
-void heapify(int heap[], int idx)
-{
-    while(idx < 0)
-    {
-        int p = parent(idx);
-        if(heap[p] > heap[idx]) swap(heap[p], heap[idx]);
-        idx = p;
-    }
-}
-
-int build_heap(int heap[], int n)
-{
-    for(int i = (n-1)/2; i >= 0; --i)
-    {
-        heapify(heap, idx, n);
-    }
-}
-
 int merge_lists(int* arr[], int k, int sz, int a[], int nsz)
 {
-    if(arr == NULL || ret == NULL) return ARGUMENT_ERROR;
+    if(arr == NULL || a == NULL) return ARGUMENT_ERROR;
 
     //initialize the pos that track each list
     int* k_pos = new int(k);
@@ -154,10 +125,79 @@ int merge_lists(int* arr[], int k, int sz, int a[], int nsz)
     delete heap;
     return SUCCESS;
 }
+*/
+
+int get_min(int a[], int i, int n)
+{
+    int min =  i;
+
+    int l_c = 2*i + 1;
+    int r_c = 2*i + 2;
+
+    if(l_c < n && a[l_c] < a[i])
+    {
+        min = l_c;
+    }
+
+    if(r_c < n && a[r_c] < a[min])
+    {
+        min = r_c;
+    }
+    return min;
+}
+
+void swap(int& a, int& b)
+{
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
+int parent(int idx)
+{
+    if(idx % 2 == 0) return (idx-1)/2;
+    else return idx/2;
+}
+
+void heapify(int a[], int i, int n)
+{
+    while(i < n)
+    {
+        int min = get_min(a, i, n);
+        if(min == i) break;
+
+        swap(a[min], a[i]);
+        i = min;
+    }
+}
+
+void build_heap(int a[], int n)
+{
+    for(int idx = (n-1)/2; idx >= 0; --idx)
+    {
+        heapify(a, idx, n);
+    }
+}
+
+/*
+bool is_heap(int a[],  int n)
+{
+    for(int idx = 0; idx <= n/2; ++idx)
+    {
+        if(a[idx] > a[2*idx+1] || a[idx] > a[2*idx+2]){
+            return false;
+        }
+    }
+    return true;
+}
+*/
 
 int main()
 {
+    int a[] = {42, 12, 9, 14, 21, 22, 56, 32, 6, 18};
+    print_array(a, 10);
+    build_heap(a, 10);
+    print_array(a, 10);
     return 0;
 }
-
 
