3820034935a9dda58a001bdf5c1b56c0be0848c5
gnbbgys <gnbbgys@gmail.com>
2017-03-04 22:49:16 -0500

    update

--- merge.cpp
+++ merge.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 
 void print_matrix(int** a, int k, int p)
 {
@@ -21,33 +22,67 @@ void print_array(int a[], int n)
     std::cout << std::endl; 
 }
 
-int main()
+int find_min(int a[][], int p[], int k, int n)
 {
-    int* c = new int[10];
-    memset(c, 0, 10*sizeof(int));
-    print_array(c, 10);
-    delete [] c;
+    int min = a[0][p[0]];
+    int lis = 0;
 
-    int* a = new int(10);
-    std::cout << *a << std::endl; 
-    delete a;
-
-    int** b = new int* [10];
-    int pos = 0;
-    int sample[] = {1, 2, 3, 4, 5};
-    for(pos = 0; pos < 10; ++pos)
+    for(int l = 1; l < k; ++l)
     {
-        b[pos] = new int [5];
-        memcpy(b[pos], sample, sizeof(int)*5);
+        if(p[l] >= n) continue;
+
+        if( min > a[l][p[l]] )
+        {
+            min = a[l][p[l]];
+            lis = l;
+        }
     }
 
-    print_matrix(b, 10, 5);
-    for(pos = 0; pos < 10; ++pos)
+    p[lis]++;
+    return min;
+}
+
+void mergekarray(int a[][], int k, int n, int b[])
+{
+    int* p = new int [k]; //track the position of each list
+    memset(p, 0, sizeof(int)*k);
+
+    int count = 0;
+    while(count < k*n)
+    {
+        int min = find_min(a, p, k, n);
+        b[count++] = min;
+    }
+
+    delete [] p;
+}
+
+int main()
+{
+    int** b = new int* [4];
+    for(int idx = 0; idx < 4; 
+    b[0] = new int [5]
+
+    b[0] = {2, 9, 12, 42, 56};
+    b[1] = {1, 4, 15, 26, 89};
+    b[2] = {6, 9, 19, 52, 78};
+    b[3] = {4, 17, 23, 21, 65};
+
+    print_matrix(b, 4, 5);
+    int* r = new int [20];
+
+    mergekarray(b, 4, 5, r);
+    print_matrix(b, 4, 5);
+
+    //delete 
+    //std::for_each(); transform
+    for(int pos = 0; pos < 10; ++pos)
     {
         delete [] b[pos];
     }
 
     delete [] b;
+    delete [] r;
 
     return 0;
 }
