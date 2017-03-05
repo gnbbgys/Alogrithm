4d5d6cae28e3a671562313e06c30ab0e0046ba3c
gnbbgys <gnbbgys@bloomberg.net>
2017-03-05 18:05:23 -0500

    update

--- merge.cpp
+++ merge.cpp
@@ -1,5 +1,8 @@
-#include <iostream>
+
 #include <string.h>
+#include <stdlib.h>
+
+#include <iostream>
 
 void print_matrix(int** a, int k, int p)
 {
@@ -44,87 +47,6 @@ int find_min(int a[][], int p[], int k, int n)
     return min;
 }
 
-void mergekarray(int a[][], int k, int n, int b[])
-{
-    int* p = new int [k]; //track the position of each list
-    memset(p, 0, sizeof(int)*k);
-
-    int count = 0;
-    while(count < k*n)
-    {
-        int min = find_min(a, p, k, n);
-        b[count++] = min;
-    }
-
-    delete [] p;
-}
-
-int main()
-{
-    int** b = new int* [4];
-    for(int idx = 0; idx < 4; 
-    b[0] = new int [5]
-
-    b[0] = {2, 9, 12, 42, 56};
-    b[1] = {1, 4, 15, 26, 89};
-    b[2] = {6, 9, 19, 52, 78};
-    b[3] = {4, 17, 23, 21, 65};
-
-    print_matrix(b, 4, 5);
-    int* r = new int [20];
-
-    mergekarray(b, 4, 5, r);
-    print_matrix(b, 4, 5);
-
-    //delete 
-    //std::for_each(); transform
-    for(int pos = 0; pos < 10; ++pos)
-    {
-        delete [] b[pos];
-    }
-
-    delete [] b;
-    delete [] r;
-
-    return 0;
-}
-
-//======================
-enum{
-    SUCCESS = 0, 
-    MEMORY_ALLOC_ERROR,
-    ARGUMENT_ERROR
-};
-
-int merge_lists(int* arr[], int k, int sz, int a[], int nsz)
-{
-    if(arr == NULL || a == NULL) return ARGUMENT_ERROR;
-
-    //initialize the pos that track each list
-    int* k_pos = new int(k);
-    memset(k_pos, 1, sizeof(int)*k);
-
-    //1. build heap size k
-    int* heap = new int(k);
-    if (heap == NULL) return MEMORY_ALLOC_ERROR; //error code
-    memset(heap, 0, sizeof(int)*k);
-
-    //copy elements to array
-    int i = 0; 
-    while(i < k)
-    {
-        heap[i] = arr[i][0];
-        i++;
-    }
-
-    //2. remove min from heap and fill in the elements from list
-    build_heap(heap, k);
-
-    //3. heapify the heap
-
-    delete heap;
-    return SUCCESS;
-}
 */
 
 int get_min(int a[], int i, int n)
@@ -192,12 +114,96 @@ bool is_heap(int a[],  int n)
 }
 */
 
+
+//============MERGE LIST==============//
+enum{
+    SUCCESS = 0, 
+    MEMORY_ALLOC_ERROR,
+    ARGUMENT_ERROR
+};
+
+int mergekarray(int* arr[], int k, int sz, int r[])
+{
+    if(arr == NULL || r == NULL) return ARGUMENT_ERROR;
+
+    //initialize the pos that track each list
+    int* k_pos = new int(k);
+    memset(k_pos, 1, sizeof(int)*k);
+
+    //1. build heap size k
+    int* heap = new int(k);
+    if (heap == NULL) return MEMORY_ALLOC_ERROR; //error code
+    memset(heap, 0, sizeof(int)*k);
+
+    //copy elements to array
+    int i = 0; 
+    while(i < k)
+    {
+        heap[i] = arr[i][0];
+        i++;
+    }
+
+    //2. remove min from heap and fill in the elements from list
+    build_heap(heap, k);
+
+    //3. heapify the heap
+
+    delete heap;
+    return SUCCESS;
+}
+
+void generate_random_array(int mod, int arr[], int sz)
+{
+    if(sz <= 0) return;
+
+    int idx = 0;
+    for(; idx < sz; idx++)
+    {
+        arr[idx] = rand() % mod;
+    }
+}
+
+int** create_matrix(int row, int col)
+{
+    int** b = new int* [row];
+
+    for(int i = 0; i < row; ++i)
+    {
+        b[i] = new int [col];
+        generate_random_array(100, b[i], col);
+    }
+
+    return b;
+}
+
+void delete_matrix(int** b, int row)
+{
+    for(int i = 0; i < row; ++i)
+    {
+        delete [] b[i];
+    }
+    delete [] b;
+}
+
 int main()
 {
     int a[] = {42, 12, 9, 14, 21, 22, 56, 32, 6, 18};
     print_array(a, 10);
     build_heap(a, 10);
     print_array(a, 10);
+
+    std::cout << " ===================== " << std::endl; 
+
+    int** b = create_matrix(4, 5);
+    print_matrix(b, 4, 5);
+
+    int* r = new int [4*5];
+    mergekarray(b, 4, 5, r);
+
+    print_array(r, 4*5);
+    delete_matrix(b, 4);
+    delete [] r;
+
     return 0;
 }
 
